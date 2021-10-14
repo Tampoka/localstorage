637317d211147c5ee3070e09e1028e36b28dee8d
Tampoka <goal.thanks@gmail.com>
2021-10-15 08:56:49 +0900

    added initial state, type

---
+++ src/BLL/counter-reducer.ts
@@ -0,0 +1,10 @@
+const initialState={
+    value:0
+}
+
+type InitialStateType=typeof initialState
+
+export const counterReducer=(state:InitialStateType=initialState,action:any):InitialStateType=>{
+    return state
+}
+
