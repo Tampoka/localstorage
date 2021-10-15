e7c1f3ee9bd3487264d0eda8ea06663e0d1ae3dc
Tampoka <goal.thanks@gmail.com>
2021-10-15 09:53:08 +0900

    added case "INC-VALUE"

--- src/BLL/counter-reducer.ts
+++ src/BLL/counter-reducer.ts
@@ -4,8 +4,15 @@ const initialState={
 
 type InitialStateType=typeof initialState
 
-export const counterReducer=(state:InitialStateType=initialState,action:any):InitialStateType=>{
-    return state
+export const counterReducer=(state:InitialStateType=initialState,action:IncValuesActionType):InitialStateType=>{
+    switch (action.type) {
+        case "INC-VALUE":
+            return {
+                ...state,value: state.value+1
+            }
+        default:
+            return state
+    }
 }
 
 export const incValueAC=()=>({type:'INC-VALUE'}as const)
